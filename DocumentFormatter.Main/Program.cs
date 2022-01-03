0580677f8c4ac513ae7086cdf31f0a94a228e14e
Strahinja Šerbula <strahinja.serbula@htecgroup.com>
2022-01-03 13:38:57 +0100

    Add project files.

---
+++ DocumentFormatter.Main/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DocumentFormatter.Main
+{
+    /// <summary>
+    /// Main execution class
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+            .BuildServiceProvider();
+        }
+    }
+}
